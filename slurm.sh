b523a174cbded676adfa9fff495b0cc634222b4f
TJLightning16 <69230683+jaiminpatel1601@users.noreply.github.com>
2025-04-01 00:39:06 -0400

    added the slurm.sh file

---
+++ slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=def-someuser
+#SBATCH --gres=gpu:4         # Number of GPU(s) per node
+#SBATCH --cpus-per-task=16         # CPU cores/threads
+#SBATCH --mem=20G               # memory per node
+#SBATCH --time=0-12:00:00         # time (DD-HH:MM)
+#SBATCH --output=kd_model.out    # output file name (%j expands to jobId)
+
+module load python cuda scipy-stack
+source ./env/bin/activate
+
+
+python -m ./csc490.py
